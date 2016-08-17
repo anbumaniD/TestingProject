5214f3eb4f4b59f4363338d40fa92a2db4e4ae3e
anbumanid <newanbu22@yahoo.com>
2016-08-17 12:09:07 +0530

    Initial Commit

---
+++ Testing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Testing
+//
+//  Created by Anbumani on 17/08/16.
+//  Copyright (c) 2016 HCL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Testing/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Testing
+//
+//  Created by Anbumani on 17/08/16.
+//  Copyright (c) 2016 HCL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
